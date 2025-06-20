89fc137e9e1c082be2fa7f1fd9a0d48ced6a473c
viniciushoffmanndev <viniciushoffmanndeveloper@gmail.com>
2025-06-19 23:32:32 -0300

    Adicionado Desafio5.ipynb ao projeto

---
+++ paths.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+DATA_RAW = BASE_DIR / 'data' / 'raw'
+DATA_PROCESSED = BASE_DIR / 'data' / 'processed'
+NOTEBOOKS = BASE_DIR / 'notebooks'
+SCRIPTS = BASE_DIR / 'scripts'
